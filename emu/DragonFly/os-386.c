e0f1da4e214cd25e6e622c098c7b6b567888e82f
Venkatesh Srinivas <me@endeavour.zapto.org>
2010-11-05 12:29:22 -0400

    DragonFly: Move osblock()/osready() to os-386.c as they are arch-specific (XADD).

--- emu/DragonFly/os-386.c
+++ emu/DragonFly/os-386.c
@@ -20,3 +20,38 @@ decref(Ref *r)
 
     return x;
 }
+
+// osblock and osready
+//
+// osblock blocks an Inferno kproc until it is woken by a corresponding
+// osready. We use the per-process os field (p->os) as a three-state counter
+// to avoid lost osready() calls; block decrements this counter, while
+// ready increments it. On the 0 -> -1 transition in block, we use
+// DragonFly's umtx_sleep syscall to wait for the counter to be 0 again.
+// On a 1 -> 0 transition in block, we 'consume' a ready event and return.
+// In ready, on the 0 -> 1 transition, we return, while we use umtx_wakeup
+// on a -1 -> 0 transition.
+//
+void
+osblock(void)
+{
+        int val;
+
+        val = _xadd(&up->os, -1);
+        if (val == 1)
+                return;
+
+        while((int) up->os == -1)
+                umtx_sleep(&up->os, -1, 0);
+}
+
+void
+osready(Proc *p)
+{
+        int val;
+
+        val = _xadd(&p->os, 1);
+        if (val == -1)
+                umtx_wakeup(&p->os, 1);
+}
+
